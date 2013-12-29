d4217c8dedb98facef6886014d35107c8440b13c
Huam Erica <erica@Erica2.local>
2013-12-29 19:51:19 +0100

    aleejo a oublié de modifié les appels a calcul_length quand il a introduit calcul_length_litt

--- load_daz.c
+++ load_daz.c
@@ -1,5 +1,3 @@
-
-
 int calcul_length_litt(unsigned char *tmp,int i ){
   return ((256*256*tmp[i+1])+(256*tmp[i+2])+tmp[i+3]);
 }
@@ -25,154 +23,110 @@ int calcul_length(unsigned char *tmp,int i ){
 	return (tmp[i+1]+(256*tmp[i+2])+(256*256*tmp[i+3]));
 }
 
-
-
-dazibao *load_daz(unsigned char *tmp, int deb,int taille){	
-	
+dazibao *load_daz(unsigned char *tmp, int deb,int taille){		
   dazibao * result ;
   result = (dazibao *) malloc (sizeof (dazibao));	
   int i=deb;
   int j,k,length=0;
-  unsigned	char *val;
+  unsigned char *val;
 
-	printf("\nTaille: %d  MBZ\n ", taille); 
+  printf("\nTaille: %d  MBZ\n ", taille); 
   while(i<taille){
 	  tlv * courant = (tlv*) malloc(sizeof(tlv));
-    // printf("apres while i= %d taille = %d \n",i,taille);
   
-    if(tmp[i]==0) {
-		courant->type = 0;
-		courant->length = 0;
-//      printf("\n Version : %d \n",tmp[i]); 
-//      printf("Pad1\n"); 
-      i++;
-    }
-    //tlv
-    else if(tmp[i]== 1 ){
-		courant->type = 1;
-		courant->length = 0;
-//      printf("\n Version :");
-//      printf("TLV PadN :  length MBZ "); i=i+3;
-    }
-  
-    //Text
-    else if(tmp[i]== 2){
-		courant->type = 2;
-		//printf("\n Type : %d \n",tmp[i]);
-		//printf(" Text version \n");
-		length=calcul_length(tmp,i);
-		 //printf("tataille :  length : %i  %d\n",length,tmp[i]);
-		i=i+3;
-		length=length+i;
-		courant->length = length;
-		i++;
-		val = malloc(sizeof(char)*length);
-		k=0;
-      for(j=i;j<=length;j++){
-		  *(val+(k++))=tmp[j];
-		  i++;
+	  if(tmp[i]==0) {
+	    courant->type = 0;
+	    courant->length = 0;
+	    i++;
 	  }
-		courant->value = val;
-		//printf("\n --> %s",courant->value );
-    }
-  
-    //PNG
-    else if(tmp[i]== 3){    
-      //printf("\n Type : %d \n",tmp[i]);
-      length=calcul_length(tmp,i);
-      i=i+3;   
-      //printf("Image PNG :  length : %i  %d\n",length,tmp[i]);
-      //length=length+i;
-      i=i+length+1;
-      //printf("\n");    
-    }
-
-    //JPEG
-    else if(tmp[i]== 4){    
-      length=calcul_length(tmp,i);
-      //printf("\n Type : %d \n",tmp[i]);
-      i=i+3;  
-      //printf("Image JPEG :  length : %i  i= %d\n",length,tmp[i]);
-      i=i+length+1;
-      //printf("\n");    
-    }
-
-
-    //COMPOUND
-    else if(tmp[i]== 5){
-      //printf("\n Type : %d \n",tmp[i]);
-      length=calcul_length(tmp,i);
-      i=i+3;    
-      //printf("TLV Compound :  length : %i  version: %d  \n",length,tmp[i+1]);
-      length=length+i;
-      load_daz(tmp,i+1,taille);
-      i = i +length+1;
-     // printf(".... %d \n",i);      
-    }
-
-    //DATE  
-    else if(tmp[i]== 6){
-   //   printf("\n Date Type : %d \n",tmp[i]);
-      //affiche DATE 
-      length=calcul_length(tmp,i);
-      i=i+7;
-      length=length+i;
-      load_daz(tmp,i+1,length);
-      i = i +length+1;
-    }else {
+	  //tlv
+	  else if(tmp[i]== 1 ){
+	    courant->type = 1;
+	    courant->length = 0;
+	  }
+	  //Text
+	  else if(tmp[i]== 2){
+	    courant->type = 2;
+	    length=calcul_length_litt(tmp,i);
+	    i=i+3;
+	    length=length+i;
+	    courant->length = length;
+	    i++;
+	    val = malloc(sizeof(char)*length);
+	    k=0;
+	    for(j=i;j<=length;j++){
+	      *(val+(k++))=tmp[j];
+	      i++;
+	    }
+	    courant->value = val;
+	  }
+	  //PNG
+	  else if(tmp[i]== 3){
+	    length=calcul_length_litt(tmp,i);
+	    i=i+3;
+	    i=i+length+1;
+	  }
+	  //JPEG
+	  else if(tmp[i]== 4){    
+	    length=calcul_length_litt(tmp,i);
+	    i=i+3;
+	    i=i+length+1;
+	  }
+	  //COMPOUND
+	  else if(tmp[i]== 5){
+	    length=calcul_length_litt(tmp,i);
+	    i=i+3;
+	    length=length+i;
+	    load_daz(tmp,i+1,taille);
+	    i = i +length+1; 
+	  }
+	  //DATE  
+	  else if(tmp[i]== 6){
+	    //affiche DATE 
+	    length=calcul_length_litt(tmp,i);
+	    i=i+7;
+	    length=length+i;
+	    load_daz(tmp,i+1,length);
+	    i = i +length+1;
+	  }else {
 		i ++;
-	}
-
-  
-  }//fin_while(1)
-	return result;
+	  }
+  }
+  return result;
 }
 
-
-
- int load_daz1(unsigned char *tmp, int deb,int taille){
-	
+int load_daz1(unsigned char *tmp, int deb,int taille){
 	dazibao * result ;
 	result = (dazibao *) malloc (sizeof (dazibao));	
 	int i=deb,cpt=0,cpt_pads=0;
 	int j,k,length=0;
-	//tlv * debut;
-	//tlv * fin;
 	unsigned char *val;
 	time_t date;
 	
 	printf("\nTaille: %d  MBZ1\n ", taille); 
 	while(i<taille){
 		tlv * courant = (tlv*) malloc(sizeof(tlv));
-		// printf("apres while i= %d taille = %d \n",i,taille);
-		
+		printf("I=%d // TAILLE=%d\n",i,taille);
 		if(tmp[i]==0) {
 		  printf("NB TLV pad1: %d\n",++cpt_pads); 
-			courant->type = 0;
-			courant->length = 0;
-			//      printf("\n Version : %d \n",tmp[i]); 
-			//      printf("Pad1\n"); 
-			i++;
+		  courant->type = 0;
+		  courant->length = 0;
+		  i++;
 		}
 		//tlv
 		else if(tmp[i]== 1){
-		  printf("NB TLV padN: %d\n",++cpt_pads);//attention
-			courant->type = 1;
-			length = calcul_length(tmp, i);
-			courant->length = length;
-			//      printf("\n Version :");
-			//      printf("TLV PadN :  length MBZ ");
-			i=i+length;
+		  printf("NB TLV padN: %d\n",++cpt_pads);
+		  courant->type = 1;
+		  length = calcul_length_litt(tmp, i);
+		  courant->length = length;
+		  i=i+length;
 		}
-		
 		//Text
 		else if(tmp[i]== 2){
 			printf("\nNB TLV: %d \n",++cpt);
 			courant->type = 2;
 			printf("Type : %d  Text\n",tmp[i]);
-			//printf(" Text version \n");
-			length=calcul_length(tmp,i);
-			//printf("tataille :  length : %d  %d\n",length,tmp[i]);
+			length=calcul_length_litt(tmp,i);
 			i=i+3;
 			length=length+i;
 			courant->length = length;
@@ -186,68 +140,54 @@ dazibao *load_daz(unsigned char *tmp, int deb,int taille){
 			courant->value = val;
 			printf(" --> %s \n",courant->value );
 		}
-		
 		//PNG
 		else if(tmp[i]== 3){    
 		  printf("\nNB TLV: *** %d ***\n",++cpt);
-			printf("Type : %d \n",tmp[i]);
-			length=calcul_length(tmp,i);
-			i=i+3;   
-			printf("Image PNG :  length : %i  \n",length);
-			//length=length+i;
-			i=i+length+1;
-			//printf("\n");    
+		  printf("Type : %d \n",tmp[i]);
+		  length=calcul_length_litt(tmp,i);
+		  i=i+3;   
+		  printf("Image PNG :  length : %i  \n",length);
+		  i=i+length+1;
 		}
-		
 		//JPEG
 		else if(tmp[i]== 4){    
 		  printf("\nNB TLV:*** %d ***\n",++cpt);
-			length=calcul_length(tmp,i);
-			printf("Type : %d \n",tmp[i]);
-			i=i+3;  
-			printf("Image JPEG :  length : %i  \n",length);
-			i=i+length+1;
-			//printf("\n");    
+		  length=calcul_length_litt(tmp,i);
+		  printf("Type : %d \n",tmp[i]);
+		  i=i+3;  
+		  printf("Image JPEG :  length : %i  \n",length);
+		  i=i+length+1;
 		}
-		
-		
 		//COMPOUND
 		else if(tmp[i]== 5){
 		  printf("\nNB TLV: *** %d ***\n",++cpt);
-			printf("Type : %d \n",tmp[i]);
-			length=calcul_length(tmp,i);
-			i=i+3;    
-			printf("TLV Compound :  length : %i  version: %d  \n",length,tmp[i+1]);
-			length=length+i;
-			load_daz1(tmp,i+1,taille);
-			i = i +length+1;
-			printf(".... %d END COMPOUND %d\n",i,cpt);      
+		  printf("Type : %d \n",tmp[i]);
+		  length=calcul_length_litt(tmp,i);
+		  i=i+3;    
+		  printf("TLV Compound :  length : %i  version: %d  \n",length,tmp[i+1]);
+		  length=length+i;
+		  load_daz1(tmp,i+1,taille);
+		  i = i +length+1;
+		  printf(".... %d END COMPOUND %d\n",i,cpt);      
 		}
-		
 		//DATE  
 		else if(tmp[i]== 6){
 		  printf("\nNB TLV: *** %d ***\n",++cpt);
-			printf("Type : %d \n Date : ",tmp[i]);
-			//Calcul longueur 3 octets (i+3)
-			length=calcul_length(tmp,i);
-			i=i+3;
-			//calcul date sur 4 octets i+4
-			date = calcul_datebige(tmp,i);
-			//Affichage DATE 
-			affiche_date(date);
-			i=i+4;
-			length=length+i;
-			load_daz1(tmp,i+1,length);
-			i = i +length+1;
-			printf(".... %d END DATE %d\n",i,cpt);  
+		  printf("Type : %d \n Date : ",tmp[i]);
+		  //Calcul longueur 3 octets (i+3)
+		  length=calcul_length_litt(tmp,i);
+		  i=i+3;
+		  //calcul date sur 4 octets i+4
+		  date = calcul_datebige(tmp,i);
+		  //Affichage DATE 
+		  affiche_date(date);
+		  i=i+4;
+		  length=length+i;
+		  load_daz1(tmp,i+1,length);
+		  i = i +length+1;
+		  printf(".... %d END DATE %d\n",i,cpt);  
 		}
-		else {
-			i ++;
-		}
-		
-	}//fin_while(1)
+		printf("I=%d // TAILLE=%d\n",i,taille);
+	}
 	return cpt;
 }
-
-
-
